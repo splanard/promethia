2326276e7604e02b993aecb3db9c3334d427dcde
splanard <sebastien.planard@gmail.com>
2019-06-21 18:18:30 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,8 @@ function read_dataset( $path ){
 		'y_trues' => []
 	];
 	if( ($handle = fopen($path, "r")) !== FALSE ){
-		while( ($data = fgetcsv($handle, 160, ",")) !== FALSE ){
+		while( ($data = fgetcsv($handle, 200, ",")) !== FALSE ){
+			array_shift($data); // we don't need the first column (date)
 			$set['y_trues'][] = array_pop( $data );
 			$set['data'][] = $data;
 		}
@@ -17,23 +18,22 @@ function read_dataset( $path ){
 	}
 	return $set;
 }
-$train = read_dataset('resources/train_sample_norm.csv');
-$test = read_dataset('resources/test_sample_norm.csv');
+$train = read_dataset('resources/train_sample.csv');
+//echo var_export($train['data'], true);
+$test = read_dataset('resources/test_sample.csv');
 
 // Create the NN
-//$network = FullyConnectedNeuralNetwork::create(18, [18], 1);
-$network = FullyConnectedNeuralNetwork::fromConf(file_get_contents("resources/conf/conf[2].txt"));
+$network = FullyConnectedNeuralNetwork::create(19, [19], 1);
+//$network = FullyConnectedNeuralNetwork::fromConf(file_get_contents("resources/conf/conf[2].txt"));
 
 // Train the NN
-/*
-$network->train($train['data'], $train['y_trues'], 0.5, 1000);
-$network->autoTrain($train['data'], $train['y_trues']);
-$conf = $network->exportConf();
-file_put_contents("export.txt", $conf);
-*/
+//$network->train($train['data'], $train['y_trues'], 0.5, 1000);
+$network->autoTrain($train['data'], $train['y_trues'], '10 minute');
+//$conf = $network->exportConf();
+//file_put_contents("export.txt", $conf);
 
 // Test the NN
-$biggest_fire_2018 = [0.72727,0.73333,1.00000,0.65108,0.87230,0.62230,0.82716,0.01994,0.23647,0.03419,0.19373,0.08262,0.31624,0.36,0.69,0.00000,-0.00047,-0.00064,0.37627];
-echo $network->feedforward($biggest_fire_2018)[0].PHP_EOL;
+//$biggest_fire_2018 = [0.72727,0.73333,1.00000,0.65108,0.87230,0.62230,0.82716,0.01994,0.23647,0.03419,0.19373,0.08262,0.31624,0.36,0.69,0.00000,-0.00047,-0.00064,0.37627];
+//echo $network->feedforward($biggest_fire_2018)[0].PHP_EOL;
 
-echo "loss on the test dataset: ".$network->test($test['data'], $test['y_trues']).PHP_EOL;
+//echo "loss on the test dataset: ".$network->test($test['data'], $test['y_trues']).PHP_EOL;
