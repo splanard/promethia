921c914b7cb8d4c2d7011e3a465da2dac2b8bd50
splanard <sebastien.planard@gmail.com>
2019-06-21 12:17:34 +0200

    Add the boolean column 'fire' to the sample

--- resources/sql/2-populate_promethee.sql
+++ resources/sql/2-populate_promethee.sql
@@ -1,2 +1,6 @@
 -- fire_starts & fire_surface
 update sample s set s.fire_starts = (select count(surface) from promethee where cast(alerte as date) = s.date), s.fire_surface = (select sum(surface) from promethee where cast(alerte as date) = s.date)
+
+-- fire
+update sample set fire = 0;
+update sample set fire = 1 where fire_surface > 0;
